d00fda7221239af622a5cbe80aad7ccc5c619840
abhishekgadhia1 <abhishek.gadhia@gmail.com>
2023-01-24 22:19:17 +0530

    second commit

--- src/Arraylist.java
+++ src/Arraylist.java
@@ -1,26 +1,50 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Arraylist
 {
 	public static void main(String[] args)
 	{
-		ArrayList alist = new ArrayList();
-		alist.add("Apple");
-		alist.add("Banana");
+		ArrayList<String> alist = new ArrayList<String>();
 		
-		System.out.println(alist);
+		Scanner scan = new Scanner(System.in);
 		
-		alist.remove("Banana");
-		System.out.println(alist);
-		
-		alist.add(1, "Chiccoo");
-		System.out.println(alist);
-		
-		if(alist.contains("Apple"))
+		while(true)
 		{
-			System.out.println("It's there");
-		}
+		System.out.println("1. Add item to list");
+		System.out.println("2. Remove item from list");
+		System.out.println("3. Print list");
+		System.out.println("4. Exit");
 		
-		System.out.println(alist.size());
+		int choice = scan.nextInt();
+		scan.nextLine();
+		
+		switch(choice)
+		{
+			case 1 : System.out.print("Enter item : ");
+			String item = scan.nextLine();
+			alist.add(item);
+			break;
+			
+			case 2 : System.out.print("Enter item : ");
+			item = scan.next();
+			alist.remove(item);
+			break;
+			
+			case 3 : System.out.println("To-Do list : ");
+			for(String list : alist)
+			{
+				System.out.println(list);
+			}
+						break;
+			
+			case 4 : //System.out.print("Enter item : ");
+			System.exit(0);
+			
+			
+			default : System.out.println("Invalid choice. Please try again.");
+			break;
+		}
+		}
 	}
 }
